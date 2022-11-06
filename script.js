c50cb1a487119a02242f68a53130826c7ccf2302
datakvitsiani <datuka.colt@gmail.com>
2022-11-06 19:49:48 +0400

    finished

---
+++ script.js
@@ -0,0 +1,91 @@
+'use strict'; 
+
+// დავ. 1
+// let array = [5, 25, 89, 120, 36];
+
+// array.push('javascript', 'python')
+// array.unshift('html', 'css')
+// array.pop()
+// array.shift()
+
+// console.log(array.length);
+
+// დავ. 2 
+
+// let array = ['orange', 'banana', 'peach'];
+// array.push('apple', 'pineapple')
+// array.unshift('watermelon')
+// array.splice(3,1, 'mango')
+// array.shift()
+// array.pop()
+// console.log(array.length);
+
+// დავ. 3
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// let newArray = array.map( (item) =>  item / 3);
+
+// console.log(newArray);
+
+// დავ. 4 
+
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let result = array.filter()
+// console.log(result);
+
+// დავ. 5
+
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+// let newLanguages = languages.filter((y) => y.length > 3 );
+// console.log(newLanguages);
+
+// დავ. 6
+
+
+// დავ. 7 
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+
+// let newWords = words.filter( y => y.includes('m', 'M'));
+// console.log(newWords);
+
+// დავ. 8 
+
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+
+// let newArr = arr1.concat(arr2,arr3)
+// console.log(newArr);
+
+// დავ. 9 
+
+// let arr = [1, 2, 3, 4, 5];
+
+// დავ. 10 
+
+// let numbers = [-2, 5, 10, -5, -1, 1, 3, 4, -15];
+
+// let newNumbers = numbers.filter((y) => y > 0);
+// console.log(newNumbers);
+
+// დავ. 11 
+
+// let arr = [-1, -2, -3, 4].some(function(y) {
+//       return y > 0;
+// })
+// console.log(arr);
+
+// დავ. 12 
+
+// let array =[23,45,32,5,87,7,3,98]
+
+// დავ. 13 
+
+// let array = [1, 2, 3, 4, 5];
+
+// array.splice(3,0, 'a', 'b', 'c')
+// console.log(array);
